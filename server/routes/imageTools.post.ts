fb5be9513ff6b3427792dfb205878589eb82b035
rocke3 <rfmamun0@gmail.com>
2022-09-10 02:53:39 +0600

    File Path log

--- server/routes/imageTools.post.ts
+++ server/routes/imageTools.post.ts
@@ -1,21 +1,34 @@
 import imagemin from "imagemin";
-import formidable from "formidable";
+//import formidable from "formidable";
+import { IncomingForm } from "formidable";
 import { defineEventHandler } from "h3";
 
 export default defineEventHandler(async (event) => {
-	const form = formidable();
+	const form = new IncomingForm();
 	const req = event.req,
 		res = event.res;
-	console.log("in");
 
-	form.parse(req, async (err, fields, files) => {
-		const fileInfo = JSON.parse(JSON.stringify(files));
-		const image = fileInfo.maxImg[0];
-
-		console.log(err);
-		console.log(image);
+	form.on("file", (field, file) => {
+		// Do something with the file
+		// e.g. save it to the database
+		// you can access it using file.path
+		//originalFilename
+		console.log("file", file.filepath);
 	});
+	form.on("end", () => {
+		console.log("end");
+	});
+	form.parse(req);
+
+	// form.parse(req, async (err, fields, files) => {
+	// 	const fileInfo = JSON.parse(JSON.stringify(files));
+	// 	const image = fileInfo.maxImg[0];
+
+	// 	console.log(image);
+	// });
 	//const outImg = await imagemin([image.filepath]);
+
+	return "asd";
 });
 
 // export default async (req, res, next) => {
