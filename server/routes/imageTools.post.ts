d48f4649af39f1af6abb34675ec0900bc9f9d4ca
rocke3 <rfmamun0@gmail.com>
2022-09-13 20:19:11 +0600

    caniuse-lite

--- server/routes/imageTools.post.ts
+++ server/routes/imageTools.post.ts
@@ -1,40 +1,28 @@
-import imagemin from "imagemin";
-import { IncomingForm } from "formidable";
-import { defineEventHandler } from "h3";
+// import imagemin from "imagemin";
+// import { defineEventHandler } from "h3";
 
 // export default defineEventHandler(async (event) => {
-// 	const form = new IncomingForm({ multiples: true });
+// 	const req = event.req;
+// 	console.log(req);
+
+// 	return req;
+// });
+
+// //file.filepath
+
+// import { IncomingForm } from "formidable";
+// import { Request, Response } from "express";
+
+// export function upload(req: Request, res: Response) {
+// 	const form = new IncomingForm();
 // 	form.on("file", (field, file) => {
 // 		// Do something with the file
 // 		// e.g. save it to the database
-// 		// originalFilename , mimetype, filepath, newFilename
-// 		console.log("file", file.filepath);
+// 		// you can access it using file.path
+// 		console.log("file", file.name);
 // 	});
-// 	form.on("end", (err) => {
-// 		console.log(err);
+// 	form.on("end", () => {
+// 		res.json();
 // 	});
-// 	form.parse(event.req);
-// 	//const outImg = await imagemin([image.filepath]);
-
-// 	return "asd";
-// });
-
-export default (req, res, next) => {
-	const form = new IncomingForm();
-	form.multiples = true;
-	form.maxFileSize = 50 * 1024 * 1024; // 5MB
-	form.uploadDir = "./";
-	form.on("file", async (field, file) => {
-		// Do something with the file
-		// e.g. save it to the database
-		// originalFilename , mimetype, filepath, newFilename
-		console.log("file", file.filepath);
-		const outImg = await imagemin([file.filepath]);
-		console.log(outImg[0].data);
-		return outImg[0].data;
-	});
-	form.on("end", (data) => {
-		console.log(data);
-	});
-	form.parse(req);
-};
+// 	form.parse(req);
+// }
