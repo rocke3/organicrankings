5eb7d8544e0327983f1f14adf3a0057c1d328b75
rocke3 <rfmamun0@gmail.com>
2022-09-19 02:05:14 +0600

    login issue fix

--- pages/app/imagetools.vue
+++ pages/app/imagetools.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, watch } from 'vue'
+import { ref } from 'vue'
 import axios from 'axios'
 import formData from 'form-data'
 definePageMeta({ layout: "app-layout" });
--- server/middleware/UserAuth.ts
+++ server/middleware/UserAuth.ts
@@ -1,4 +1,4 @@
-import { env } from "node:process";
+const env = useRuntimeConfig();
 import { defineEventHandler, getCookie } from "h3";
 import jwt from "jsonwebtoken";
 import md5 from "md5";
