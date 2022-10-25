85f728c1dd6491d46d93a6d7eda07cc576dcc9e6
rocke3 <rfmamun0@gmail.com>
2022-10-26 00:19:24 +0600

    update

--- server/routes/webhook.post.ts
+++ server/routes/webhook.post.ts
@@ -1,5 +1,5 @@
 const env = useRuntimeConfig();
-import { createError, defineEventHandler, getHeader, readRawBody } from "h3";
+import { defineEventHandler, getHeader, readRawBody } from "h3";
 import db from "../connection";
 import Stripe from "stripe";
 const stripe = new Stripe(env.stripeSk, { apiVersion: "2022-08-01" });
@@ -19,25 +19,33 @@ export default defineEventHandler(async (req) => {
 	// Handle the event
 	switch (event.type) {
 		case "checkout.session.completed":
-			const session = event.data.object;
+			const payment = event.data.object;
 			return await db
 				.promise()
-				.query("UPDATE `subscriptions` SET `sub_subscription` = ?, `sub_active`= 1 WHERE `sub_session` = ?", [session.subscription, session.id])
+				.query("UPDATE `subscriptions` SET `sub_subscription` = ?, `sub_active`= 1, `sub_end` = ? WHERE `sub_session` = ?", [payment.subscription, payment.id, payment.id])
 				.then(([rows, fields]) => {
 					return "Activated";
 				})
 				.catch((error) => {
 					return error;
 				});
-
 			break;
 		case "customer.subscription.updated":
-			const subscription = event.data.object;
-			return subscription;
+			const updated = event.data.object;
+			return await db
+				.promise()
+				.query("UPDATE `subscriptions` SET `sub_end`= ? WHERE `sub_subscription` = ?", [updated.current_period_end, updated.id])
+				.then(([rows, fields]) => {
+					return "Activated";
+				})
+				.catch((error) => {
+					return error;
+				});
+			return updated;
 			break;
 		case "customer.subscription.deleted":
-			const subscription2 = event.data.object;
-			return subscription2;
+			const deleted = event.data.object;
+			return deleted;
 			break;
 
 		// ... handle other event types
