beafa76d2cd5ef0f4d7e2f330e1f5a20be98ce3a
rocke3 <rfmamun0@gmail.com>
2022-11-25 03:18:19 +0600

    asdasd

--- pages/user/texttools.vue
+++ pages/user/texttools.vue
@@ -106,7 +106,7 @@ watch(text, async (string) => {
                           {{ key }}
                         </div>
                       </td>
-                      <td class="value">{{ value }} ({{ (value / totalWords * 100).toFixed(0) }}%)</td>
+                      <td class="value">{{ value }} ({{ (value / totalWords * 100).toFixed(1) }}%)</td>
                     </tr>
                     <tr>
                       <td colspan="2" class="text-center" v-if="uniqueWords > 5">
