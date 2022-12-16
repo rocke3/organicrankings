afa028b22ffa97d10e908b379e22296b216827cb
rocke3 <rfmamun0@gmail.com>
2022-12-16 21:15:27 +0600

    reset password

--- pages/resetpassword.vue
+++ pages/resetpassword.vue
@@ -6,38 +6,49 @@ useHead({
 });
 definePageMeta({ layout: "public-layout" });
 
-const form = ref({ email: '', confemail: '', captchaValid: false, })
+const formResponse = ref('')
+const email = ref('')
+const confemail = ref('')
+const captchaValid = ref(false)
 
 async function passwordResetRequst(form) {
-	if (form.email && form.confemail && form.captchaValid && form.email == form.confemail) {
-		axios.post('/requstSignup', { email: form.email, confemail: form.confemail })
+	captchaValid.value = true
+	if (email.value && confemail.value && email.value == confemail.value) {
+		axios.post('/passwordresetrequst', { email: email.value, confemail: confemail.value })
 			.then(async function (res) {
 				let data = res.data;
-
+				if (res.data)
+					formResponse.value = `<span class='text-success'><i class='material-icons statusIcon'>check_circle</i>A reset link has been sent to your email</span>`
+				else
+					formResponse.value = `<span class='text-danger'><i class='material-icons statusIcon'>warning</i>Something went wrong please try again later</span>`
 			}).catch((error) => {
-				console.log(error);
+				formResponse.value = `<span class='text-danger'><i class='material-icons statusIcon'>warning</i>Something went wrong please try again later</span>`
 			});
 
 	} else {
-
+		formResponse.value = `<span class='text-danger'><i class='material-icons statusIcon'>warning</i>Confemail Email dosen't match</span>`
 	}
+	email.value = ''
+	confemail.value = ''
+	setTimeout(function () { formResponse.value = ''; captchaValid.value = false }, 3000);
 }
 </script>
 
 <template>
 	<div v-once>
 		<ElementsSingCard formTitle="Reset Password" titleClass="font-weight-bolder text-center text-uppercase h3">
-			<form role="form" class="text-start" @submit.prevent="passwordResetRequst(form)">
-				<ElementsInputEmail label="Email" v-model:email="form.email" :required="true" />
-				<ElementsInputEmail label="Confirm Email" v-model:email="form.confemail" class="mt-4" :required="true" />
+			<form role="form" class="text-start" @submit.prevent="passwordResetRequst()">
+				<ElementsInputEmail label="Email" v-model:email="email" :required="true" />
+				<ElementsInputEmail label="Confirm Email" v-model:email="confemail" class="mt-4" :required="true" />
 				<div class="mt-3">
 					<ClientOnly fallback-tag="span" fallback="Loading Captcha . . . . .">
-						<ElementsCaptchaInput v-model:captchaValid="form.captchaValid" />
+						<ElementsCaptchaInput v-model:captchaValid="captchaValid" />
 					</ClientOnly>
 				</div>
 				<div class="text-center mt-2">
 					<button type="submit" class="btn btn-primary w-100 mt-3">Reset</button>
 				</div>
+				<div v-html="formResponse" class="text-center"></div>
 				<SsrLinks :reset="false" />
 			</form>
 		</ElementsSingCard>
--- server/routes/passwordresetrequst.post.ts
+++ server/routes/passwordresetrequst.post.ts
@@ -1,19 +1,38 @@
 import { defineEventHandler, readBody } from "h3";
-
+const env = useRuntimeConfig();
+import md5 from "md5";
+import axios from "axios";
+import db from "../connection";
 export default defineEventHandler(async (event) => {
 	const body = await readBody(event);
 	const email = body.email;
 	const confemail = body.confemail;
 
-	//Validate
-	if (!email.match(/^([\w-\.]{2,4})+@([\w-]{2,4})+\.+[\w-]{2,4}$/) || !confemail.match(/^([\w-\.]{2,4})+@([\w-]{2,4})+\.+[\w-]{2,4}$/) || email != confemail) {
+	//Validate email format
+	if (!email.match(/^([\w-\.]{2,4})+@([\w-]{2,4})+\.+[\w-]{2,4}$/) || !confemail.match(/^([\w-\.]{2,4})+@([\w-]{2,4})+\.+[\w-]{2,4}$/)) {
 		return { login: false, message: "Invalid Email" };
 	}
 
 	if (email && confemail && email == confemail) {
-		return { login: false, message: "Email or Password dosen't match " };
+		return await db
+			.promise()
+			.query("SELECT user_email FROM users WHERE user_email = ?", [email])
+			.then(([rows, fields]) => {
+				if (rows[0]) {
+					const time = new Date().getTime();
+					const user_reset = md5(email + time);
+					db.promise().query("UPDATE users SET user_reset = ? WHERE user_email = ?", [user_reset, email]);
+					var params = new URLSearchParams({ form: "resetPass", email: email, hash: user_reset });
+					axios.post("https://www.organicrankings.com/sendmail/", params);
+					return true;
+				} else {
+					return false;
+				}
+			})
+			.catch((error) => {
+				return { login: false, message: "Something went wrong please try again later", error: error };
+			});
 	} else {
-		return { login: false, message: "Email or Password dosen't match " };
+		return { login: false, message: "Confemail Email dosen't match" };
 	}
-	return { login: false, message: "Something went wrong please try again later" };
 });
