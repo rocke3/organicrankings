1e5041a7465a9d96b18733fc39030819b2ffd79d
rocke3 <rfmamun0@gmail.com>
2023-01-07 00:58:59 +0600

    asd

--- server/routes/getUserInvoice.ts
+++ server/routes/getUserInvoice.ts
@@ -3,6 +3,14 @@ import stripe from "../stripe";
 
 export default defineEventHandler(async (req) => {
 	let body = await readBody(req);
-
-	return await stripe.getInvoice(body.invoice);
+	let invoice = await stripe.getInvoice(body.invoice);
+	let paymentIntent: any;
+	if (invoice.payment_intent) paymentIntent = await stripe.getPaymentIntent(invoice.payment_intent);
+	return {
+		amount: paymentIntent.amount / 100,
+		url: invoice.hosted_invoice_url,
+		pdf: invoice.invoice_pdf,
+		paid: invoice.paid,
+		status: invoice.status,
+	};
 });
